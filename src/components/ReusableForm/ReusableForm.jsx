8dc6de02ee56a640f3967c5361f4c69d9b72d732
MdAfsarHossain <afsar291722@gmail.com>
2024-10-09 16:53:37 +0600

    Reusable Form jsx file added

---
+++ src/components/ReusableForm/ReusableForm.jsx
@@ -0,0 +1,39 @@
+
+const ReusableForm = ({formTitle, handleSubmit, submitBtnText="Submit", children}) => {
+
+    // const handleSubmit = (e) => {
+        // e.preventDefault();
+        // console.log(e.target.name.value)
+        // console.log('Form submitted');
+    // }
+
+    const handleLocalSubmit = (e) => {
+        e.preventDefault();
+
+        const data = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            password: e.target.password.value
+        }
+
+        handleSubmit(data);
+    }
+
+    return (
+        <div>
+            {/* <h2>{formTitle}</h2> */}
+            {children}
+            <form onSubmit={handleLocalSubmit}>
+                <input type="text" name="name" />
+                <br />
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="password" name="password"/>
+                <br />
+                <input type="submit" value={submitBtnText} />
+            </form>
+        </div>
+    );
+};
+
+export default ReusableForm;
